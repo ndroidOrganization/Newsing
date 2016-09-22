4713bae89d652bceb77fe3fc8c0ea26a3c7e194d
Zhu Quan <qzhu@helixnt.com.cn>
2016-09-22 10:20:47 +0800

    merge commit

--- app/src/main/java/com/newsing/utils/http/JsonUtils.java
+++ app/src/main/java/com/newsing/utils/http/JsonUtils.java
@@ -1,35 +1,35 @@
-package com.newsing.utils.http;
-
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONException;
-import org.androidannotations.annotations.EBean;
-import org.json.JSONObject;
-
-import java.util.List;
-
-/**
- * Created by Angel on 2016/8/25.
- */
-@EBean
-public class JsonUtils {
-
-    public String getJson(JSONObject o) {
-        return o.toString();
-    }
-
-    public <T> T getObject(String jsonObject, Class<T> t) {
-        try {
-            return JSON.parseObject(jsonObject, t);
-        } catch (JSONException e) {
-            return null;
-        }
-    }
-
-    public <T> List<T> getArray(String jsonArray, Class<T> t) {
-        try {
-            return JSON.parseArray(jsonArray, t);
-        } catch (JSONException e) {
-            return null;
-        }
-    }
-}
+//package com.newsing.utils.http;
+//
+//import com.alibaba.fastjson.JSON;
+//import com.alibaba.fastjson.JSONException;
+//import org.androidannotations.annotations.EBean;
+//import org.json.JSONObject;
+//
+//import java.util.List;
+//
+///**
+// * Created by Angel on 2016/8/25.
+// */
+//@EBean
+//public class JsonUtils {
+//
+//    public String getJson(JSONObject o) {
+//        return o.toString();
+//    }
+//
+//    public <T> T getObject(String jsonObject, Class<T> t) {
+//        try {
+//            return JSON.parseObject(jsonObject, t);
+//        } catch (JSONException e) {
+//            return null;
+//        }
+//    }
+//
+//    public <T> List<T> getArray(String jsonArray, Class<T> t) {
+//        try {
+//            return JSON.parseArray(jsonArray, t);
+//        } catch (JSONException e) {
+//            return null;
+//        }
+//    }
+//}
